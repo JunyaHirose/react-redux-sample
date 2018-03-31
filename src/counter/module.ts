9debb7a83526f65af71494d881716920440e489f
uryyyyyyy <koki305@gmail.com>
2018-03-31 14:36:32 +0900

    async

--- src/counter/module.ts
+++ src/counter/module.ts
@@ -3,6 +3,8 @@ import {Action} from 'redux'
 enum ActionNames {
   INC = 'counter/increment',
   DEC = 'counter/decrement',
+  FETCH_START = 'counter/fetch_request_start',
+  FETCH_FINISH = 'counter/fetch_request_finish'
 }
 
 interface IncrementAction extends Action {
@@ -21,20 +23,44 @@ interface DecrementAction extends Action {
   appAction: boolean
   minusAmount: number
 }
-
 export const decrementAmount = (amount: number): DecrementAction => ({
   type: ActionNames.DEC,
   appAction: true,
   minusAmount: amount
 })
 
+interface FetchRequestStartAction extends Action {
+  type: ActionNames.FETCH_START
+  appAction: boolean
+}
+export const fetchRequestStart = (): FetchRequestStartAction => ({
+  type: ActionNames.FETCH_START,
+  appAction: true,
+})
+
+interface FetchRequestFinishAction extends Action {
+  type: ActionNames.FETCH_FINISH
+  appAction: boolean
+}
+export const fetchRequestFinish = (): FetchRequestFinishAction => ({
+  type: ActionNames.FETCH_FINISH,
+  appAction: true,
+})
+
 export interface CounterState {
   num: number
+  loadingCount: number
 }
 
-export type CounterActions = IncrementAction | DecrementAction
+export type CounterActions = IncrementAction
+  | DecrementAction
+  | FetchRequestStartAction
+  | FetchRequestFinishAction
 
-const initialState:CounterState = {num: 0}
+const initialState:CounterState = {
+  num: 0,
+  loadingCount: 0
+}
 
 export default function reducer(state: CounterState = initialState, action: Action | CounterActions): CounterState {
   if (!("appAction" in action)) {
@@ -42,9 +68,15 @@ export default function reducer(state: CounterState = initialState, action: Acti
   }
   switch (action.type) {
     case ActionNames.INC:
-      return {num: state.num + action.plusAmount}
+      return Object.assign({}, state, {num: state.num + action.plusAmount})
     case ActionNames.DEC:
-      return {num: state.num - action.minusAmount}
+      return Object.assign({}, state, {num: state.num - action.minusAmount})
+    case ActionNames.FETCH_START: {
+      return Object.assign({}, state, {loadingCount: state.loadingCount + 1})
+    }
+    case ActionNames.FETCH_FINISH: {
+      return Object.assign({}, state, {loadingCount: state.loadingCount - 1})
+    }
     default:
       return state
   }
